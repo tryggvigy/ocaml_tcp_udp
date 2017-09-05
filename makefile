42613cb5bacf382c0e5a3f47c6437c42922473c5
Tryggvi Gylfason <tryggvi@spotify.com>
2017-09-05 15:03:46 +0200

    introduce rawsocket.ml

--- makefile
+++ makefile
@@ -4,8 +4,11 @@ build_tcp:
 	ocamlc -g -custom -o tcp.out unix.cma tcp.ml -cclib -lunix
 build_udp:
 	ocamlc -g -custom -o udp.out unix.cma udp.ml -cclib -lunix
+build_raw:
+	ocamlc -g -custom -o rawsocket.out unix.cma rawsocket.ml -cclib -lunix
 clean:
 	ocamlbuild -clean
 tcp: build_tcp clean
 udp: build_udp clean
+raw: build_raw clean
 .PHONY: udp
--- rawsocket.ml
+++ rawsocket.ml
@@ -3,9 +3,12 @@
 let maxlen = 1024
 let portno = 8080
 
+exception Error of string;;
+
 let sock =
-  Unix.socket Unix.PF_INET Unix.SOCK_DGRAM
-    (Unix.getprotobyname "udp").Unix.p_proto
+  try Unix.socket Unix.PF_INET Unix.SOCK_RAW 0
+  with Unix.Unix_error(e, s1, s2) ->
+    raise(Error("Error: " ^ Unix.error_message(e) ^ s1 ^ s2))
 
 let resolve_host server =
   let server_addr =
@@ -18,25 +21,22 @@ let resolve_host server =
 
 let () =
   Unix.bind sock (Unix.ADDR_INET (resolve_host "localhost", portno));
-  Printf.printf "Awaiting UDP messages on port %d\n%!" portno
+  Printf.printf "Awaiting raw ethernet frames on port %d\n%!" portno
 
 let () =
   let buf = Bytes.create maxlen in
   while true do
     let response, hishost, sockaddr =
       match Unix.recvfrom sock buf 0 maxlen [] with
-      | len, (Unix.ADDR_INET (addr, port) as sockaddr) ->
-        String.sub buf 0 len,
-        (Unix.gethostbyaddr addr).Unix.h_name,
-        sockaddr
+      | len, (Unix.ADDR_INET (addr, port) as sockaddr) -> String.sub buf 0 len, (Unix.gethostbyaddr addr).Unix.h_name, sockaddr
       | _ -> assert false in
     Printf.printf "Client %s said: %s\n%!" hishost (String.uppercase response);
-    Unix.sendto 
-      sock
-      (String.uppercase response) 
-      0
-      (String.length response) 
-      []
-      sockaddr
-    |> ignore;
+    (* Unix.sendto 
+       sock
+       (String.uppercase response) 
+       0
+       (String.length response) 
+       []
+       sockaddr
+       |> ignore; *)
   done
